971d1b8fa9a49b13188330fbf1495f59a6ce786a
Muhammad Al Debaisy <mouradmounir93@gmail.com>
2020-09-18 13:15:09 +0200

    Filteration tree

--- src/app/modules/products/components/large-filter-aside/large-filter-aside.component.ts
+++ src/app/modules/products/components/large-filter-aside/large-filter-aside.component.ts
@@ -1,16 +1,65 @@
+import { NestedTreeControl } from '@angular/cdk/tree';
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
+
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Fruit',
+    children: [
+      {name: 'Apple'},
+      {name: 'Banana'},
+      {name: 'Fruit loops'},
+    ]
+  }, {
+    name: 'Vegetables',
+    children: [
+      {
+        name: 'Green',
+        children: [
+          {name: 'Broccoli'},
+          {name: 'Brussels sprouts'},
+        ]
+      }, {
+        name: 'Orange',
+        children: [
+          {name: 'Pumpkins'},
+          {name: 'Carrots'},
+        ]
+      },
+    ]
+  },
+];
+
 
 @Component({
   selector: 'app-large-filter-aside',
   templateUrl: './large-filter-aside.component.html',
   styleUrls: ['./large-filter-aside.component.scss']
 })
+
 export class LargeFilterAsideComponent implements OnInit {
+  
+  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<FoodNode>();
 
-  constructor() { }
-
-  ngOnInit(): void {
-
+  constructor() {
+    this.dataSource.data = TREE_DATA;
   }
 
+  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
+  ngOnInit(): void {
+     
+  }
+  selectedCategory(selectedCategory){
+    console.log(selectedCategory)
+  }
 }
---
+++ src/app/modules/products/services/filteration.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, Inject } from '@angular/core'; 
+import { environment } from '../../../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, from } from 'rxjs'; 
+  
+@Injectable({
+  providedIn: 'root'
+})
+export class FilterationService{
+    
+    constructor(){
+        
+    }
+     
+}
